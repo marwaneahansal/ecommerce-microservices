a04d01930b0dfcba0c1e283c4679acfedf5af44d
Marwane Ahansal <ahansalmarwane@gmail.com>
2023-12-05 16:32:56 +0100

    init commit

---
+++ gateway/src/index.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+import express from 'express';
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world!');
+});
